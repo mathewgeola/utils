c44cf80b62924600c6b699e5359ad58c073f9b2c
mathewgeola <mathewgeola@gmail.com>
2025-10-31 14:17:00 +0800

    version = "v0.0.5"

--- src/utils/_file.py
+++ src/utils/_file.py
@@ -5,33 +5,61 @@ from typing import Literal, cast
 
 
 class _file:
+    COMPRESS_TYPE = Literal["zip"]
+
     @staticmethod
-    def compress_dir_path(
+    def compress(
             dir_path: str,
-            compress_type: Literal["zip"] = "zip"
+            compress_file_path: str | None = None,
+            compress_type: COMPRESS_TYPE = "zip"
     ) -> str | None:
         dir_path = os.path.abspath(dir_path)
-
         if not os.path.isdir(dir_path):
             return None
 
-        valid_compress_type = ["zip"]
-        if compress_type not in valid_compress_type:
-            return None
-
-        compress_file_path = os.path.join(
-            os.path.dirname(dir_path), os.path.basename(dir_path) + "." + compress_type
-        )
+        if compress_file_path is None:
+            compress_file_path = os.path.join(
+                os.path.dirname(dir_path), os.path.basename(dir_path) + "." + compress_type
+            )
+        else:
+            compress_file_path = os.path.abspath(compress_file_path)
 
         if compress_type == "zip":
             with zipfile.ZipFile(compress_file_path, "w", zipfile.ZIP_DEFLATED) as zf:
                 for root, dirs, files in os.walk(dir_path):
                     for file in files:
                         file_path = os.path.join(root, file)
+
+                        if os.path.abspath(file_path) == compress_file_path:
+                            continue
+
                         rel_file_path = os.path.relpath(file_path, dir_path)
                         zf.write(file_path, arcname=rel_file_path)
-
             return compress_file_path
+
+        return None
+
+    @staticmethod
+    def decompress(
+            compress_file_path: str,
+            dir_path: str | None = None,
+            compress_type: COMPRESS_TYPE = "zip"
+    ) -> str | None:
+        compress_file_path = os.path.abspath(compress_file_path)
+        if not os.path.isfile(compress_file_path):
+            return None
+
+        if dir_path is None:
+            dir_path = os.path.splitext(compress_file_path)[0]
+        else:
+            dir_path = os.path.abspath(dir_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        if compress_type == "zip":
+            with zipfile.ZipFile(compress_file_path, "r") as zf:
+                zf.extractall(dir_path)
+            return dir_path
 
         return None
 
--- tests/test_file.py
+++ tests/test_file.py
@@ -2,7 +2,8 @@ import utils
 
 
 def test_file():
-    print(utils.file.compress_dir_path("."))
+    print(utils.file.compress(".", "tests.zip"))
+    print(utils.file.decompress("tests.zip", "tests"))
     print(utils.file.get_file_paths_and_dir_paths("."))
 
 
